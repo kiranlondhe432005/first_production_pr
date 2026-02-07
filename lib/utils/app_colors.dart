d5a5801cb3224f61b1653ecb7e0aea730b17fa5a
Om Tawade <om.tawade@dypic.in>
2026-02-07 14:54:53 +0530

    Add files via upload

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF2E6CF6);
+  static const border = Color(0xFFE0E0E0);
+  static const greyButton = Color(0xFFD6D6D6);
+}
